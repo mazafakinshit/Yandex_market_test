2f6cf1fbcd6dcd3172776abf57ffdfd2e306c9eb
mazafakinshit <mazafakinshit@ya.ru>
2020-07-05 20:39:10 +0300

    successful yandex market search added steps

--- src/test/java/tests/YandexMarket_Tests.java
+++ src/test/java/tests/YandexMarket_Tests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static helpers.Environment.yandexMarketItemName;
+import static io.qameta.allure.Allure.step;
 
 @Epic("QA.GURU automation course")
 @Story("Yandex tests")
@@ -18,14 +19,20 @@ class YandexMarket_Tests extends TestBase {
     @Test
     @Description("Checking item can be found")
     void checkItemCanBeFound() {
-        open("http://market.yandex.ru");
-
-        $("html").shouldHave(text("Яндекс.Маркет"));
-        $("#header-search").val(yandexMarketItemName).pressEnter();
-        $x("//h3/a[1]").click();
-        switchTo().window(1);
-
-        $("html").shouldHave(text(yandexMarketItemName));
+        step("Заходим на Яндекс Маркет", () -> {
+            open("http://market.yandex.ru");
+        });
+        step("Проверяем наличие текста Яндекс.Маркет", () -> {
+            $("html").shouldHave(text("Яндекс.Маркет"));
+        });
+        step("Передаем название запрашиваемого товара из Environment и переходим к результатам поиска", () -> {
+            $("#header-search").val(yandexMarketItemName).pressEnter();
+            $x("//h3/a[1]").click();
+            switchTo().window(1);
+        });
+        step("Проверяем наличие переданного текста в результатах поиска", () -> {
+            $("html").shouldHave(text(yandexMarketItemName));
+        });
     }
 }
 
